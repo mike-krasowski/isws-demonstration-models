3237eccdd28d016ec62cf061ca165e92e9f5611a
Zavelle <78878271+Zavelle@users.noreply.github.com>
2025-08-06 12:30:00 -0500

    Update az-test.py
    has notes on things to work on

--- layered_groundwater_model/az-test.py
+++ layered_groundwater_model/az-test.py
@@ -1,4 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import flopy
+
 text = 'ATTICUS >> FEDS!'
 
 for i in range(10000):
+    print(2+2)
     print(text)
+
+
+
+# IDEAS:
+#   - load model results
+#   - query model results (e.g., pressures at well locations)
+#   - plot time series of pressure/flow rate in model
