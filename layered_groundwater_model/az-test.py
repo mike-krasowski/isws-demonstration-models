620b86780cbb15ccdc1f8eea9bfd076c627e2b98
Zavelle <78878271+Zavelle@users.noreply.github.com>
2025-08-13 10:52:57 -0500

    Update az-test.py
    messed around.  save progress

--- layered_groundwater_model/az-test.py
+++ layered_groundwater_model/az-test.py
@@ -2,11 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import flopy
 
-text = 'ATTICUS >> FEDS!'
+file_path = "C:\GitHub\isws-demonstration-models\layered_groundwater_model\outputs\s1\gwf_s1.dis"  # Replace with your actual file path
 
-for i in range(10000):
-    print(2+2)
-    print(text)
+try:
+    with open(file_path, 'r') as file:
+        # Read the entire content of the file
+        content = file.read()
+        print("File content:")
+        print(content)
+
+        # Or read line by line
+        #for line in file:
+         # print(line.strip()) # .strip() removes leading/trailing whitespace including newlines
+
+except FileNotFoundError:
+    print(f"Error: The file at '{file_path}' was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+
+#text = 'ATTICUS >> FEDS!'
+
+#for i in range(10000):
+ #   print(2+2)
+  #  print(text)
 
 
 
