379fbafff56fb95fff97dca5d473f8355b16a436
allanejones <alljones@illinois.edu>
2025-09-03 11:34:47 -0500

    Update az-test.py
    Cleaning and listing of ideas for next steps.

--- layered_groundwater_model/az-test.py
+++ layered_groundwater_model/az-test.py
@@ -21,32 +21,8 @@ import flopy.utils.binaryfile as bf
 import pandas as pd
 hds = bf.HeadFile(path2).get_alldata() # (time, layer, row, column)
 
-print ("squeeks")
-
 # read in well csv --> pandas as pd --> pd.read_csv
 well = pd.read_csv('./inputs/well_info.csv')
-print (well)
-
-
-
-# # determine well locations and cells from csv
-# xyz = gwf.modelgrid.xyzcellcenters
-# coords = well.loc[
-#     ((well['type']=='well') & (well['id']=='well_2')),
-#     ['id','x_coord', 'z_coord']
-# ]
-# print(coords)
-#
-# x_bin = int(coords.iloc[0]['x_coord'] // gwf.modelgrid.delr[0])
-# z_bin = int(coords.iloc[0]['z_coord'] // gwf.modelgrid.delz[0,0,0])
-# print(x_bin)
-# print(z_bin)
-#
-# # plot time series at each location --> plt.plot(hds[time, z, y(into page), x])
-# plt.plot(hds[:, z_bin, 0, x_bin])
-# plt.show()
-#
-# print('Script Finished.')
 
 # determine well locations and cells from csv
 xyz = gwf.modelgrid.xyzcellcenters
@@ -54,18 +30,25 @@ coords = well.set_index('type')
 
 x_bin = coords.loc['well']['x_coord'] // gwf.modelgrid.delr[0]
 z_bin = coords.loc['well']['z_coord'] // gwf.modelgrid.delz[0,0,0]
-print(x_bin)
-print(z_bin)
 
 # plot time series at each location --> plt.plot(hds[time, z, y(into page), x])
 plt.figure()
 for z, x, name in zip(z_bin, x_bin, coords.loc['well']['id']):
     plt.plot(hds[:, int(z), 0, int(x)], label=name)
+plt.xlabel('Time')
+plt.ylabel('Heads [cm]')
 plt.legend(loc='lower right')
 plt.show()
 
 print('Script Finished.')
 
+
+# IDEAS:
+#   X - load model results
+#   X - query model results (e.g., pressures at well locations) (gwf)
+#   X - plot time series of pressure/flow rate in model (gwf)
+#   - plot breakthrough curves (concentration over time) for all wells (gwt)
+#         --> UCN file is concentrations
 
 
 
@@ -95,8 +78,4 @@ except Exception as e:
 
 
 
-# IDEAS:
-#   - load model results
-#   - query model results (e.g., pressures at well locations) (gwf)
-#   - plot time series of pressure/flow rate in model (gwf)
-#   - plot breakthrough curves (gwt)
+
