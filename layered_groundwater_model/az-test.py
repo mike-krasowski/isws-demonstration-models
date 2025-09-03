b1f0e0d5a3ce0596daa4220a3fd76e6253850ebf
Zavelle <78878271+Zavelle@users.noreply.github.com>
2025-09-03 10:32:39 -0500

    Update az-test.py
    played around- commit changes for meeting

--- layered_groundwater_model/az-test.py
+++ layered_groundwater_model/az-test.py
@@ -19,27 +19,37 @@ path2 = r".\outputs\s1\gwf_s1.hds"
 import flopy.utils.binaryfile as bf
 import pandas as pd
 hds = bf.HeadFile(path2).get_alldata() # (time, layer, row, column)
-# print (hds)
-# print("soy")
-# df = pd.DataFrame(hds)
-# df.to_csv("heads_data.csv", index=None)
+
 print ("squeeks")
 
 # read in well csv --> pandas as pd --> pd.read_csv
 well = pd.read_csv('./inputs/well_info.csv')
+print (well)
+'''
 # determine well locations and cells from csv
 xyz = gwf.modelgrid.xyzcellcenters
 coords = well.loc[
-    (well['id']=='well_1'),
+    (well['id']=='well_2'),
     ['x_coord', 'z_coord']
 ]
+'''
+
+# determine well locations and cells from csv
+xyz = gwf.modelgrid.xyzcellcenters
+coords = well.loc[
+    ((well['type']=='well') & (well['id']=='well_2')),
+    ['id','x_coord', 'z_coord']
+]
+print (coords)
 x_bin = int(coords['x_coord'][0] // gwf.modelgrid.delr[0])
 z_bin = int(coords['z_coord'][0] // gwf.modelgrid.delz[0,0,0])
-
+print (x_bin)
+print (z_bin)
+'''
 # plot time series at each location --> plt.plot(hds[time, z, y(into page), x])
 plt.plot(hds[:, z_bin, 0, x_bin])
 plt.show()
-
+'''
 raise Exception('AEJ breaks stuff.')
 
 '''
